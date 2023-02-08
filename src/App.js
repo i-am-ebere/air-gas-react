beea1bdce231a665174c6077ff80c96d87d593ce
schnecke <eiweala@live.com>
2023-02-08 12:34:54 -0800

    save

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import { useState } from "react";
 import "./App.css";
-import { NameTagWithChildren, NameTag } from "./NameTagWithChildren";
-import { Name } from "./Name";
-import Email from "./Email";
-import { Hobby } from "./Hobby";
 
 function App() {
-  // const [value, setterFunc] = useState(initialValue);
-  const [isOn, setIsOn] = useState(0);
-  const [birthday, setBirthday] = useState(10);
-  console.log("here we go");
+  const [todoListItems, setTodoListItems] = useState(["clean", "pick mail"]);
+
   return (
     <div className="App">
-      Is this on: {isOn ? "is on" : "is off"}
-      <br />
-      <button onClick={() => setIsOn((prevState) => prevState + 1)}>
-        Toggle isOn
-      </button>
-      <NameTag
-        listOfHobbies={["running", "jumping"]}
-        name="Ebere"
-        asdf={birthday}
-        changeBirthday={setBirthday}
-        desc={{ age: 10, pet: "cat" }}
-        getFullName={() => "Ebere I"}
-      />
-      {/* 
-      <br />
-      <br />
-      <NameTagWithChildren isLoading={false}>
-        <div>hello I am a component with children1</div>
-        <div>hello I am a component with children2</div>
-        <div>hello I am a component with children3</div>
-      </NameTagWithChildren> */}
-      {/* <Name name="Ebere I" />
-      <Email name="ebere@email.com" />
-      <Hobby name="Running" /> */}
+      <AddTodoListItem addItem={setTodoListItems} />
+      {todoListItems.map((todo, index) => (
+        <TodoListItem todoItemName={todo} key={index} />
+      ))}
     </div>
   );
+}
+
+function AddTodoListItem(props) {
+  const [todoItemValue, setTodoItemValue] = useState("");
+
+  function onChangeUserInput(e) {
+    setTodoItemValue(e.target.value);
+  }
+
+  function onSubmit() {
+    props.addItem((prevState) => [...prevState, todoItemValue]);
+  }
+
+  return (
+    <div>
+      <input onChange={onChangeUserInput} value={todoItemValue} />
+      <button onClick={onSubmit}>Add</button>
+    </div>
+  );
+}
+
+function TodoListItem(props) {
+  return <div>{props.todoItemName}</div>;
 }
 
 export default App;
