9c1ac91526fc0ca1db47a72c60bf52dac60aac42
schnecke <eiweala@live.com>
2023-02-08 10:56:15 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -12,18 +12,29 @@ function App() {
       />
       <br />
       <br />
-      <NameTagWithChildren>
-        <span>hello I am a component with children</span>
+      <NameTagWithChildren isLoading={false}>
+        <div>hello I am a component with children1</div>
+        <div>hello I am a component with children2</div>
+        <div>hello I am a component with children3</div>
       </NameTagWithChildren>
     </div>
   );
 }
 
 function NameTagWithChildren(props) {
+  function shouldDisplayLoading() {
+    if (props.isLoading) {
+      return "loading...";
+    } else {
+      return <div>{props.children}</div>;
+    }
+  }
+
   return (
     <div>
       <div>Hello NameTagWithChildren</div>
-      <div>{props.children}</div>
+      {/* adding it to a function {shouldDisplayLoading()} */}
+      {props.isLoading ? "loading..." : <div>{props.children}</div>}
     </div>
   );
 }
